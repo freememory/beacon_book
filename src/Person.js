86182be5180b2ea4dcbf6479b2644c5cae3358bd
Michael Acobas <michael.acobas@baschel.io>
2018-06-06 15:00:02 +0300

    remove empty css rule, convert to <p>

--- src/Person.js
+++ src/Person.js
@@ -4,7 +4,7 @@ const person = (props) => {
     const { name, location, byline, id, onDelete } = props;
     return (
         <Card title={name} extra={location} style={{width: 250}} actions={[<Icon type="delete" onClick={() => onDelete(id)}/>, <Icon type="edit" />]}>
-            {byline.split("\n").map((s,i) => <div key={i}>{s}</div>)}            
+            {byline.split("\n").map((s,i) => <p key={i}>{s}</p>)}            
         </Card>
     )
 }
